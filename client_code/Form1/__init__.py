ff3b461b4900e0c760441ee5f06805f6e40a0f8d
marlon.pichler@student.htl-rankweil.at <marlon.pichler@student.htl-rankweil.at>
2024-11-22 16:33:15 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -17,7 +17,7 @@ class Form1(Form1Template):
     self.label_freie_zellen.text = anvil.server.call('get_freieZellen',self.gefaengnisse_drop_down.selected_value)
     data = anvil.server.call('get_ZellenData',self.gefaengnisse_drop_down.selected_value)
     #self.repeating_zellen.items = [{'zellennummer': data[0][0], 'anzahl_häftlinge': data[0][1]}, 
-     #                            {'zellennummer': data[1][0], 'anzahl_häftlinge': data[1][1]}]
+   #                             {'zellennummer': data[1][0], 'anzahl_häftlinge': data[1][1]}]
 
   def gefaengnisse_drop_down_change(self, **event_args):
     """This method is called when an item is selected"""
